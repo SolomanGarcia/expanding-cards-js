e2034a9790733c8e2203915415ddfb22bbb513a8
Soloman Garcia <soloman.garcia@gmail.com>
2020-12-02 10:09:30 -0700

    create removeActiveClasses function

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
+    removeActiveClasses();
     panel.classList.add("active");
   });
 });
+
+function removeActiveClasses() {
+  panel.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
